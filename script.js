335b9a574f17c05e973eb9c394a2f3f9884680bd
CrashOverrider <91897336+Sankhadip-Roy@users.noreply.github.com>
2023-01-10 22:54:40 +0530

    revamp

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-// let a = prompt("hey what's your age?")
-// if (a > 18 && a < 100) {
-//     alert("Ok, you have valid age to enter")
-// }
-// else if (a < 18) {
-//     let age = a < 10 ? "less than 10" : "between 10-18"
-//     alert("you are " + age + ", shouldn't see the page content")
-// }
-// else {
-//     alert("This is an invalid age")
-// }
 
+setTimeout(function () {
+    let name = prompt("hey, what's your name ?")
+    alert("Welcome, '" + name + "' Happy to see you here 😊\nIf you are using a mobile device please activate DESKTOP MODE.")
+}, 10000);
 
-let name = prompt("hey, what's your name ?")
-alert("Welcome, '" + name + "' Happy to see you here 😊\nIf you are using a mobile device please activate desktop mode.")
+function age() {
+    let a = prompt("hey what's your age?")
+    if (a > 18 && a < 100) {
+        alert("Ok, you have valid age to enter")
+    }
+    else if (a < 18) {
+        let age = a < 10 ? "less than 10" : "between 10-18"
+        alert("you are " + age + ", shouldn't see the page content")
+    }
+    else {
+        alert("This is an invalid age")
+    }
+}
+setTimeout(age, 50000);
+
