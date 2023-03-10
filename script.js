6795696a61378d0d8d885dbcb1dd9293795fb166
[Sankhadip-Roy] <[sankhadiproy23@gmail.com]>
2023-03-11 03:18:55 +0530

    updates

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function oldWeb() {
     location.href = 'oldWebsite/oldWebsite.html'
 }
+function weatherSite() {
+    location.href = 'https://sankhadip-roy.github.io/weatherSite/'
+}
 function startIdea() {
     window.open('files/teamOnestepBusinessIdea.pptx', '_self');
 }
