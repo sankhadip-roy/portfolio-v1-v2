cd2472c5acaa2addd7b894b4226e4ae5e725ff7e
[Sankhadip-Roy] <[sankhadiproy23@gmail.com]>
2023-03-17 22:54:22 +0530

    update

--- oldWebsite/oldScript.js
+++ oldWebsite/oldScript.js
@@ -1,4 +1,3 @@
-//src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"//jquery
 // function showAlert(msg, ok) {
 //     var confirmBox = $("#confirm");
 //     confirmBox.find(".message").text(msg);
@@ -10,23 +9,60 @@
 // }
 
 function feedBack() {
-    let runAgain = true;
-    while (runAgain) {
-        let a = prompt("Rate this website from 0 to 10 !")
-        if (a >= 7 && a <= 10) {
-            alert("Thank you, for your feedback.")
-        }
-        else if (a >= 5 && a < 7) {
-            alert("Yea this is bad, I'm tring to improve this.")
-        }
-        else {
-            let lowFeedback = a < 5 ? "Sorry for your dissapointment," : "Invalid feedback,"
-            alert(lowFeedback + " I have just started learning.")
-        }
-        runAgain = confirm("Do you want to submit again!");
+    // let runAgain = true;
+    // while (runAgain) {
+    let a = prompt("Rate this website from 0 to 10 !")
+    let heading = document.querySelector('h1');
+    if (a >= 7 && a <= 10) {
+        alert(`Thanks ! it's ${a}`)
+        heading.innerHTML = `Thanks ! it's ${a}`;
     }
+    else if (a >= 5 && a < 7) {
+        alert("Yea this is bad, I'm tring to improve this.")
+        heading.innerHTML = "Yea this is bad";
+    }
+    else {
+        let lowFeedback = a < 5 ? "Sorry for your dissapointment !" : "Invalid feedback !"
+        alert(lowFeedback + " I have just started learning.")
+        heading.innerHTML = lowFeedback;
+    }
+    //     runAgain = confirm("Do you want to submit again!");
+    // }
 }
 setTimeout(feedBack, 50000);
+
+function mode() {
+    if (document.body.style.color === "green") {
+        document.body.style.color = "white";
+        document.querySelector('#footerId').style.color = button.dataset.color;
+
+    }
+    else {
+        document.body.style.color = "green"
+    }
+}
+document.querySelector('button').onclick = mode;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('button').forEach(button => {
+        button.onclick = function () {
+            document.querySelector('#hello').style.color = button.dataset.color;
+        }
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('select').onchange = function () {
+        document.querySelector('#hello').style.color = this.value;
+    }
+})
+document.addEventListener('DOMContentLoaded', () => {
+    $("#showAlert").click(function () {
+        Swal.fire(
+            "If mobile device: use a Desktop (This website isn't optimized)",
+        )
+    })
+});
 
 //Node.js
 // const os = require('os');
--- script.js
+++ script.js
@@ -2,8 +2,18 @@ function oldWeb() {
     location.href = 'oldWebsite/oldWebsite.html'
 }
 function weatherSite() {
-    location.href = 'https://sankhadip-roy.github.io/weatherSite/'
+    location.href = 'https://sankhadip-roy.github.io/WeatherSite/'
 }
 function startIdea() {
     window.open('files/teamOnestepBusinessIdea.pptx', '_self');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#form1').onsubmit = function () {
+        const email = document.querySelector('#exampleFormControlInput1').value;
+        const suggestionOn = document.querySelector('#exampleFormControlSelect1').value;
+        const suggestion = document.querySelector('#exampleFormControlTextarea1').value;
+        alert(`Your Feedback:\nEmail: ${email}\n` + `Suggesion on: ${suggestionOn}\n` + `Suggesion: ${suggestion}`);
+    }
+
+})
